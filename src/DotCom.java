f3eb5cdc54caf7e635715fb787314f24145fe042
platocha <pawel.latocha1990@gmail.com>
2021-02-21 12:04:38 +0100

    Final game

--- src/DotCom.java
+++ src/DotCom.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 
 public class DotCom {
     private ArrayList<String> locationCells;
+    private String name;
 
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
     }
 
     public String checkYourself(String userInput) {
@@ -15,6 +20,7 @@ public class DotCom {
 
             if (locationCells.isEmpty()) {
                 result = "kill";
+                System.out.println("Ouch! You sunk " + name + "!");
             } else {
                 result = "hit";
             }
