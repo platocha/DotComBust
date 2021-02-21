c4dbcebf7abc802fa81f19054009d41f3bb73b61
platocha <pawel.latocha1990@gmail.com>
2021-02-21 12:13:16 +0100

    Added tests

--- src/DotComTestDrive.java
+++ src/DotComTestDrive.java
@@ -4,18 +4,39 @@ public class DotComTestDrive {
     public static void main (String[] args) {
 
         ArrayList<String> locations = new ArrayList<String>();
-        locations.add("2");
-        locations.add("3");
-        locations.add("4");
+        locations.add("a1");
+        locations.add("a2");
+        locations.add("a3");
 
         DotCom dot = new DotCom();
+        dot.setName("test.com");
 
         dot.setLocationCells(locations);
 
-        String userGuess = "2";
+        String userGuess = "a1";
         String result = dot.checkYourself(userGuess);
         String testResult = "failed";
         if (result.equals("hit")) {
+            testResult = "passed";
+        }
+
+        System.out.println(testResult);
+
+        userGuess = "a4";
+        result = dot.checkYourself(userGuess);
+        testResult = "failed";
+        if (result.equals("miss")) {
+            testResult = "passed";
+        }
+
+        System.out.println(testResult);
+
+        userGuess = "a2";
+        result = dot.checkYourself(userGuess);
+        userGuess = "a3";
+        result = dot.checkYourself(userGuess);
+        testResult = "failed";
+        if (result.equals("kill")) {
             testResult = "passed";
         }
 
